895b1e3573e9215310cfc5130e537808d5d82f97
Alex Carvalho <Alex Carvalho>
2016-06-30 16:31:54 +0100

    feat: Add Layout - Adapter Use - Fake Data

--- app/src/main/java/com/lex/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/lex/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,30 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        String[] forecastArray = {
+                "Today - Sunny - 22/12",
+                "Tomorrow - Foggy - 18/10",
+                "Weds - Cloudy - 19/9",
+                "Thurs - Asteroids - 20/18",
+                "Fri - Heavy Rain - 12/7",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 9/5",
+                "Sun - Sunny - 23/14" };
+
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> forecastArrayAdapter =
+                new ArrayAdapter<String>(this.getActivity(),
+                                         R.layout.list_item_forecast,
+                                         R.id.list_item_forecast_textview,
+                                         weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        ListView listViewForecast = (ListView)rootView.findViewById(R.id.listview_forecast);
+
+        listViewForecast.setAdapter(forecastArrayAdapter);
+
+        return rootView;
     }
 }

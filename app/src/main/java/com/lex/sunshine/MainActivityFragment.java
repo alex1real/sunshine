a6abb27169732888aa29ed4a7b3c57f4e7333572
Alex Carvalho <Alex Carvalho>
2016-07-04 01:56:08 +0100

    feat: Get forecast from OpenWeatherMap using HTTP
    This feature is added with an error
    (android.os.NetworkOnMainThreadException)
    The forecast is gotten using HTTP. The class wich does this job is:
     HttpURLConnection

--- app/src/main/java/com/lex/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/lex/sunshine/MainActivityFragment.java
@@ -2,12 +2,20 @@ package com.lex.sunshine;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,6 +23,8 @@ import java.util.Arrays;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public static final String OPEN_WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&units=metric&mode=json&cnt=7&APPID=350389f98777014acf1168ddbef077d3";
 
     public MainActivityFragment() {
     }
@@ -45,6 +55,66 @@ public class MainActivityFragment extends Fragment {
         ListView listViewForecast = (ListView)rootView.findViewById(R.id.listview_forecast);
 
         listViewForecast.setAdapter(forecastArrayAdapter);
+
+        //Getting the weather forecast from openweathermap.org
+        HttpURLConnection httpURLConnection = null;
+        BufferedReader bufferedReader = null;
+        String forecastJsonStr = null;
+
+        try{
+            //Construct the URL for OpenWeatherMap query
+            //Possible parameters are available at http://openweathermap.org/API#forecast
+            URL url = new URL(MainActivityFragment.OPEN_WEATHER_URL);
+
+            //Create a request to OpenWeatherMap, and open the connection
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+
+            //Read the input Stream into a String
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            if(inputStream == null){
+                //Nothing to do
+                return null;
+            }
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                // Since it's a JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it makes debbugging a *lot* easier if your print out the completed
+                // buffer for debuging
+                stringBuffer.append(line + "\n");
+            }
+
+            if(stringBuffer.length() == 0){
+                // Stream was empty. No point in parsing.
+                return null;
+            }
+
+            forecastJsonStr = stringBuffer.toString();
+        }
+        catch(IOException e){
+            Log.e("PlaceholderFragment", "Error ", e);
+        }
+        finally {
+            if(httpURLConnection != null){
+                httpURLConnection.disconnect();
+            }
+
+            if(bufferedReader != null){
+                try{
+                    bufferedReader.close();
+                }
+                catch(final IOException e){
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
 
         return rootView;
     }

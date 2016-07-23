7340d0b65cad390e3f89c246298acbaa0038dc8e
Alex Carvalho <Alex Carvalho>
2016-07-23 15:44:14 +0100

    feat: Add Settings Activity

--- app/src/main/java/com/lex/sunshine/DetailActivity.java
+++ app/src/main/java/com/lex/sunshine/DetailActivity.java
@@ -1,11 +1,13 @@
 package com.lex.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
@@ -17,6 +19,20 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id = menuItem.getItemId();
+
+        if(id == R.id.action_settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 
     /***********************

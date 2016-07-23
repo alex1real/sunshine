b0f9dff86fc68dafaa4add84406357e49420eb11
Alex Carvalho <Alex Carvalho>
2016-07-23 15:28:15 +0100

    feat: Add Menu button to DetailActivity

--- app/src/main/java/com/lex/sunshine/DetailActivity.java
+++ app/src/main/java/com/lex/sunshine/DetailActivity.java
@@ -5,10 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
+    /**********
+     * Public *
+     *********/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    /***********************
+     * Auto-generated Code *
+     **********************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

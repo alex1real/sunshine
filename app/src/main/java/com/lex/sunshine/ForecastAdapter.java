128cc8be056fdee554e78e9f0d940feebdae6883
Your Name <you@example.com>
2016-12-12 05:01:06 +0000

    feat: Display 2 List Item types

--- app/src/main/java/com/lex/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/lex/sunshine/ForecastAdapter.java
@@ -18,49 +18,29 @@ import com.lex.sunshine.db.WeatherContract;
 
 public class ForecastAdapter extends CursorAdapter {
 
+    /*************
+     * Constants *
+     ************/
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
+
+    /*************
+     * Variables *
+     ************/
     private Context context;
 
-
+    /****************
+     * Constructors *
+     ***************/
     public ForecastAdapter(Context context, Cursor cursor, int flags){
         super(context, cursor, flags);
 
         this.context = context;
     }
 
-    /*
-     * Prepare the weather high/lows for presentation
-     */
-    private String formatHighLows(double high, double low, Context context){
-        boolean isMetric = Utility.isMetric(context);
-
-        String highLowStr = Utility.formatTemperature(high, isMetric)
-                + "/" + Utility.formatTemperature(low, isMetric);
-
-        return highLowStr;
-    }
-
-    /*
-     * This is ported from FetchWeatherTask -- but now we go straight from the cursor to string.
-     */
-    private String convertCursorRowToUXFormat(Cursor cursor){
-        String highAndLow = formatHighLows(cursor.getDouble(DetailActivityFragment.COL_WEATHER_MAX_TEMP),
-                cursor.getDouble(DetailActivityFragment.COL_WEATHER_MIN_TEMP), context);
-
-        return Utility.formatDate(cursor.getLong(DetailActivityFragment.COL_WEATHER_DATE))
-                + " - " + cursor.getString(DetailActivityFragment.COL_WEATHER_DESC)
-                + " - " + highAndLow;
-    }
-
-    /*
-     * Remember that this views are reused as needed.
-     */
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent){
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
-    }
-
+    /**************
+     * Overriders *
+     *************/
     /*
      * This is where we fill-in the views with the contents of the cursor.
      */
@@ -92,4 +72,56 @@ public class ForecastAdapter extends CursorAdapter {
         minTempView.setText(Utility.formatTemperature(minTemp, isMetric));
     }
 
+    @Override
+    public int getItemViewType(int position){
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount(){
+        return 2;
+    }
+
+    /*
+     * Remember that this views are reused as needed.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent){
+        // Chose the layout type
+        int viewType = this.getItemViewType(cursor.getPosition());
+
+        int layoutId = (viewType == this.VIEW_TYPE_TODAY) ? R.layout.list_item_forecast_today
+                : R.layout.list_item_forecast;
+
+        View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
+
+        return view;
+    }
+
+    /*******************
+     * Private Methods *
+     ******************/
+    /*
+     * Prepare the weather high/lows for presentation
+     */
+    private String formatHighLows(double high, double low, Context context){
+        boolean isMetric = Utility.isMetric(context);
+
+        String highLowStr = Utility.formatTemperature(high, isMetric)
+                + "/" + Utility.formatTemperature(low, isMetric);
+
+        return highLowStr;
+    }
+
+    /*
+     * This is ported from FetchWeatherTask -- but now we go straight from the cursor to string.
+     */
+    private String convertCursorRowToUXFormat(Cursor cursor){
+        String highAndLow = formatHighLows(cursor.getDouble(DetailActivityFragment.COL_WEATHER_MAX_TEMP),
+                cursor.getDouble(DetailActivityFragment.COL_WEATHER_MIN_TEMP), context);
+
+        return Utility.formatDate(cursor.getLong(DetailActivityFragment.COL_WEATHER_DATE))
+                + " - " + cursor.getString(DetailActivityFragment.COL_WEATHER_DESC)
+                + " - " + highAndLow;
+    }
 }

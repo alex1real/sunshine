832e969fab37caad235fee68fe0b2b48950b1fae
Your Name <you@example.com>
2016-12-15 03:52:44 +0000

    fix: Correct a type in Weahter.weather_id column name

--- app/src/main/java/com/lex/sunshine/db/WeatherContract.java
+++ app/src/main/java/com/lex/sunshine/db/WeatherContract.java
@@ -122,7 +122,7 @@ public class WeatherContract {
         public static final String COLUMN_LOC_KEY = "location_id";
 
         //Weather Id is returned by API, to identify the icon to be used.
-        public static final String COLUMN_WEATHER_ID = "wheater_id";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
 
         //Date, stored as long in milliseconds since the epoch
         public static final String COLUMN_DATE = "date";
